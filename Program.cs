af8ab1e7f08715b447128e4f539aecca279ec43c
learnergthb <98642809+learnergthb@users.noreply.github.com>
2022-07-13 18:58:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            udp s = new udp();
+            s.Server("0.0.0.0", 443);
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
+
+
+
+    
+
